e761921f66fd0b41ce721f08ca20c7a57a45c549
wangziyu1234 <2372025257@qq.com>
2026-04-29 17:14:52 +0800

    Remove duplicate thesis template root

--- QLUThesisLatexTemplate-master/Thesis/scripts/linux-clean.sh
+++
@@ -1,3 +0,0 @@
-find "$(dirname "$(dirname "$0")")" -regextype posix-extended \
-    -regex '.*\.(aux|log|out|thm|toc|bbl|blg|fdb_latexmk|fls)' \
-    -delete
--- QLUThesisLatexTemplate-master/Thesis/scripts/macos-clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PARENT_DIR="$(dirname "$(dirname "$0")")"
-find -E "$PARENT_DIR" -regex ".*\.(aux|log|out|thm|toc|bbl|blg|fdb_latexmk|fls|gz)" -delete
